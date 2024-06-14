3b300cae6d1d764e33b3dc6e5d183289a27c49b2
QuinnAsena <qase352@aucklanduni.ac.nz>
2024-06-14 11:28:01 -0500

    updating content

--- install.r
+++ install.r
@@ -1,2 +1,2 @@
 if (!require("pacman")) install.packages("pacman", repos="http://cran.r-project.org")
-pacman::p_load(rmarkdown, languageserver, ggplot2)    # Install & load packages
+pacman::p_load(neotoma2, rmarkdown, languageserver, ggplot2, dplyr, tidyr)    # Install & load packages

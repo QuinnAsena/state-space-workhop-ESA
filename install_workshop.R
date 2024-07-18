5a813ded69ea1275066d9f57c099d028cc2e776d
QuinnAsena <qase352@aucklanduni.ac.nz>
2024-07-18 18:41:21 -0500

    updated install files and README with instructions

--- install_workshop.R
+++ install_workshop.R
@@ -1,8 +1,31 @@
+if (!require("multinomialTS")) {
+  stop("
+    Required dependency multinomialTS.
+    See the README: https://github.com/QuinnAsena/state-space-workhop-ESA/tree/main#welcome-to-the-multinomialts-workshop ; 
+    or the instruction comments below in the install_workshop.R file (this file),
+    or try my workshop_autoinstall.R (which may or may not work...)"
+  )
+} else {
 if (!require("pacman")) install.packages("pacman", repos="http://cran.r-project.org")
-pacman::p_load(rmarkdown, languageserver, ggplot2, dplyr, tidyr, devtools, stringr, scales,
+pacman::p_load(neotoma2, rmarkdown, languageserver, ggplot2, dplyr, tidyr, devtools, stringr, scales,
                RcppArmadillo, minqa, matrixStats, numDeriv, mvtnorm, multinomialTS) # Install & load packages
+}
+
+
+# If necessary to install multinomialTS from GitHub Directly
+# Requires xcode-select on mac
+# Requires Rtools44 on windows
 # devtools::install_github("https://github.com/QuinnAsena/multinomialTS")
 
+# To install binary files for windows, does not require Rtools44
+# Requires R 4.4 or greater
 # install.packages("https://github.com/QuinnAsena/multinomialTS/releases/download/v1.0.0/multinomialTS_1.0.0.zip", repos = NULL, type = "win.binary")
 
+# To install binary files for mac, does not require xcode-select
+# Requires R 4.4 or greater
 # install.packages("https://github.com/QuinnAsena/multinomialTS/releases/download/v1.0.0/multinomialTS_1.0.0.tgz", repos = NULL, type = "mac.binary")
+
+
+# neotoma2
+# If necessary to install neotoma2 from github directly:
+# devtools::install_github('NeotomaDB/neotoma2', build_vignettes = TRUE)
--- workshop_autoinstall.R
+++ workshop_autoinstall.R
@@ -1,7 +1,11 @@
-install.packages("devtools")
+if (!require("remotes")) install.packages("remotes", repos="http://cran.r-project.org")
 # Check R version
 if (compareVersion(paste(getRversion()), "4.4.0") < 0) {
-  stop("R version needs to be 4.4 or higher.")
+  stop("
+  R version needs to be 4.4 or higher. \n
+  Would you like to try the binder?
+  https://mybinder.org/v2/gh/QuinnAsena/state-space-workhop-ESA/HEAD?urlpath=rstudio \n
+  Alternatively, try updating to R 4.4.1, and also update packages.")
 }
 
 # Check OS type
@@ -14,20 +18,20 @@ if (os_type == "unix") {
     xcode_installed <- system("xcode-select -p", intern = TRUE, ignore.stderr = TRUE)
     if (nchar(xcode_installed) > 0) {
       # If Xcode is installed
-      devtools::install_github("QuinnAsena/multinomialTS")
+      remotes::install_github("QuinnAsena/multinomialTS")
     } else {
       # If Xcode is not installed
       install.packages("https://github.com/QuinnAsena/multinomialTS/releases/download/v1.0.0/multinomialTS_1.0.0.tgz", repos = NULL, type = "mac.binary")
     }
   } else {
     # For other Unix-like systems (Linux)
-    devtools::install_github("QuinnAsena/multinomialTS")
+    remotes::install_github("QuinnAsena/multinomialTS")
   }
 }
 
 if (os_type == "windows") {
   if (dir.exists("C:\\rtools44")) {
-    devtools::install_github("QuinnAsena/multinomialTS")
+    remotes::install_github("QuinnAsena/multinomialTS")
   } else {
     install.packages("https://github.com/QuinnAsena/multinomialTS/releases/download/v1.0.0/multinomialTS_1.0.0.zip", repos = NULL, type = "win.binary")
   }
